2a81ca66a8b65ea58880cb7aee000909d9b82ed5
naims6 <naimdrive6@gmail.com>
2025-07-23 20:06:43 +0600

    feature: add overlay effect when toggle navbar

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
+// Adding Menu Bar
 let menuBar = document.getElementById("menu");
 let menuClose = document.getElementById("menu-close");
 let mobileMenu = document.getElementById("mobile-menu");
+let overlay = document.getElementById("overlay");
 
+menuBar.addEventListener("click", () => {
+  mobileMenu.classList.remove("-left-[300px]");
+  mobileMenu.classList.add("left-0");
+  overlay.classList.remove("hidden");
+});
+menuClose.addEventListener("click", () => {
+  mobileMenu.classList.remove("left-0");
+  mobileMenu.classList.add("-left-[300px]");
+  overlay.classList.add("hidden");
+});
 
-
-
-
-menuBar.addEventListener("click", () =>{
-    mobileMenu.classList.remove("-left-[300px]")
-    mobileMenu.classList.add("left-0")
-})
-menuClose.addEventListener("click", () =>{
-    mobileMenu.classList.remove("left-0")
-    mobileMenu.classList.add("-left-[300px]")
-
-})
+// Overlay
